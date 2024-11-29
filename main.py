f6a8847bad754937f1d1a977d1ab0a03a01459e4
faisalalam1234 <roastskeleton424@gmail.com>
2024-11-29 13:50:52 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+# Add a title to the Streamlit app
+st.title("Welcome to My Web - I Am Faisal Alam")
+st.header("Python")
+st.subheader("Java")
+st.markdown("C++")
+st.code("""for in range(1,5,1):
+        print("hello")
+""")
